29b964809bab7f6ba9df588034d05e5236f70cbd
kadirsa1105 <kadirs.a1105@gmail.com>
2025-07-16 19:17:26 +0300

    v0.3

--- php/install_ioncube.sh
+++ php/install_ioncube.sh
@@ -3,17 +3,11 @@
 PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
 IONCUBE_URL="https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
 
-echo "➤ ionCube indiriliyor..."
-curl -fsSL "$IONCUBE_URL" -o /tmp/ioncube.tar.gz
-tar -xzf /tmp/ioncube.tar.gz -C /tmp
+curl -fsSL "$IONCUBE_URL" -o /tmp/ioncube.tar.gz > /dev/null 2>&1
+tar -xzf /tmp/ioncube.tar.gz -C /tmp > /dev/null 2>&1
 
-echo "➤ ionCube kuruluyor..."
-EXT_DIR=$(php -i | grep extension_dir | awk '{print $NF}' | grep -o '[^/]*$')
-cp "/tmp/ioncube/ioncube_loader_lin_${PHP_VERSION}.so" "/usr/local/lib/php/extensions/no-debug-non-zts-${EXT_DIR}/"
-
-echo "➤ php.ini'ye eklenecek..."
+cp "/tmp/ioncube/ioncube_loader_lin_${PHP_VERSION}.so" "/usr/local/lib/php/extensions/no-debug-non-zts-$(php -i | grep extension_dir | awk '{print $NF}' | grep -o '[^/]*$')/" > /dev/null 2>&1
 echo "zend_extension=ioncube_loader_lin_${PHP_VERSION}.so" > /usr/local/etc/php/conf.d/00-ioncube.ini
 
 rm -rf /tmp/ioncube /tmp/ioncube.tar.gz
-
 echo "✔ ionCube kurulumu tamamlandı."
